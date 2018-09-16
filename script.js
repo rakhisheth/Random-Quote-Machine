4f9ecd92de3fb5fd77be39531dacf9ca26eb7f55
Rakhi Sheth <rakhi.sheth@outlook.com>
2018-09-16 11:48:35 +0100

    add spinner

--- script.js
+++ script.js
@@ -1,47 +1,96 @@
-let quotes = [
-  "Cogito ergo sum. (I think; therefore I am.)",
-  "When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.",
-  "Great minds discuss ideas; average minds discuss events; small minds discuss people.",
-  "If you want to live a happy life, tie it to a goal, not to people or things.",
-  "Always let your conscience be your guide!",
-  "But it ain't about how hard you hit, it’s about how hard you can get hit and keep moving forward, how much can you take and keep moving forward.",
-  "Choose a job you love, and you will never have to work a day in your life"
-];
+window.addEventListener("load", function() {
+  document.getElementById("btn").addEventListener("click", function() {
+    async function getRandomQuote() {
+      let mainId = document.getElementById("main");
+      //SPINNER
+      mainId.innerHTML = "";
+      const spinnerDiv = document.createElement("div");
+      spinnerDiv.className = "spinner";
+      mainId.appendChild(spinnerDiv);
 
-let authorNames = [
-  "Rene Descartes",
-  "Helen Keller",
-  "Eleanor Roosevelt",
-  "Albert Einstien",
-  "The Blue Fairy, Pinocchio",
-  "Rocky Balboa",
-  "Confucius"
-];
+      try {
+        const url =
+          "https://cors-anywhere.herokuapp.com/https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+        fetch(url, { cache: "no-store" })
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            let quote = data[0].content;
+            let author = data[0].title;
 
-window.onload = function() {
-  document.getElementById("btns").addEventListener("click", function() {
-    let randomNum = Math.floor(Math.random() * quotes.length);
-    document.getElementById("newQuote").innerHTML = quotes[randomNum];
-    document.getElementById("authorName").innerHTML = authorNames[randomNum];
+            mainId.removeChild(spinnerDiv);
+
+            const quoteDiv = document.createElement("div");
+            quoteDiv.className = "newQuote";
+            quoteDiv.innerHTML = quote;
+            mainId.appendChild(quoteDiv);
+
+            const authorDiv = document.createElement("div");
+            authorDiv.className = "authorName";
+            authorDiv.innerHTML = author;
+            mainId.appendChild(authorDiv);
+          });
+      } catch (error) {
+        alert("Quote not found");
+      }
+    }
+    getRandomQuote();
   });
-};
+});
 
-let getQuote = document.getElementById("").innerHTML;
+///////// WITHOUT SPINNER ///////////
 
-document
-  .getElementById("tweet-this-post")
-  .addEventListener("click", function(event) {
-    event.preventDefault();
+// window.addEventListener("load", function() {
+//   document.getElementById("btn").addEventListener("click", function() {
+//     async function getRandomQuote() {
+//       try {
+//         const url =
+//           "https://cors-anywhere.herokuapp.com/https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+//         fetch(url, { cache: "no-store" })
+//           .then(response => {
+//             return response.json();
+//           })
+//           .then(data => {
+//             document.getElementById("newQuote").innerHTML = data[0].content;
+//             document.getElementById("authorName").innerHTML = data[0].title;
+//           });
+//       } catch (error) {
+//         alert("Quote not found");
+//       }
+//     }
+//     getRandomQuote();
+//   });
+// });
 
-    let tweetedLink = window.location.href;
+///////////// OLD CODE //////////////
 
-    window.open(
-      "http://twitter.com/intent/tweet?url=" +
-        tweetedLink +
-        "&text=" +
-        getPostTitle +
-        "&via=kaidez&",
-      "twitterwindow",
-      "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0"
-    );
-  });
+// window.onload = function() {
+
+// let randomNum = Math.floor(Math.random() * quotes.length);
+
+//   document.getElementById("btn").addEventListener("click", function() {
+//     document.getElementById("newQuote").innerHTML = quotes[randomNum];
+//     document.getElementById("authorName").innerHTML = authorNames[randomNum];
+//   });
+// };
+
+// let quotes = [
+//   "Cogito ergo sum. (I think; therefore I am.)",
+//   "When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.",
+//   "Great minds discuss ideas; average minds discuss events; small minds discuss people.",
+//   "If you want to live a happy life, tie it to a goal, not to people or things.",
+//   "Always let your conscience be your guide!",
+//   "But it ain't about how hard you hit, it’s about how hard you can get hit and keep moving forward, how much can you take and keep moving forward.",
+//   "Choose a job you love, and you will never have to work a day in your life"
+// ];
+
+// let authorNames = [
+//   "Rene Descartes",
+//   "Helen Keller",
+//   "Eleanor Roosevelt",
+//   "Albert Einstien",
+//   "The Blue Fairy, Pinocchio",
+//   "Rocky Balboa",
+//   "Confucius"
+// ];
